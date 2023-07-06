a3f450400aed7484b0221c37e9eca8b5260cceab
Elizamihortaliza <cesdesigner06@gmail.com>
2023-07-06 16:42:24 -0600

    animacion

--- assets/js/main.js
+++ assets/js/main.js
@@ -215,21 +215,43 @@ progress.each(function () {
 /**
    * Typing effect
    */
-$('.typing__module').each(function (index) {
-	var self = $(this),
-		    _wrapper = $('.typed', self)[0],
-		    optData = eval('(' + self.attr('data-options') + ')'),
-		    optDefault = {
-		stringsElement: self.find('.typed-strings')[0],
-		typeSpeed: 50,
-		backSpeed: 500,
-		fadeOut: true,
-		loop: true
-	},
-		    options = $.extend(optDefault, optData);
-	var typed = new Typed(_wrapper, options);
-});
-
+if (window.isMobile) {
+	/**
+   * Typing effect
+   */
+	$('.typing__module').each(function (index) {
+		var self = $(this),
+			_wrapper = $('.typed', self)[0],
+			optData = eval('(' + self.attr('data-options') + ')'),
+			optDefault = {
+				stringsElement: self.find('.typed-strings')[0],
+				typeSpeed: 50,
+				backSpeed: 500,
+				fadeOut: true,
+				loop: false
+			},
+			options = $.extend(optDefault, optData);
+		var typed = new Typed(_wrapper, options);
+	});
+} else {
+	/**
+   * Typing effect
+   */
+	$('.typing__module').each(function (index) {
+		var self = $(this),
+			_wrapper = $('.typed', self)[0],
+			optData = eval('(' + self.attr('data-options') + ')'),
+			optDefault = {
+				stringsElement: self.find('.typed-strings')[0],
+				typeSpeed: 50,
+				backSpeed: 500,
+				fadeOut: true,
+				loop: true
+			},
+			options = $.extend(optDefault, optData);
+		var typed = new Typed(_wrapper, options);
+	});
+}
 /**
   * Footer
   */
